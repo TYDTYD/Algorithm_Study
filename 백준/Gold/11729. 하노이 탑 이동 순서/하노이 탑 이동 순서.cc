f82bf440db9c79cea42beb8ecc0d89517c778e40
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2024-03-30 20:47:08 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 128 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -2,17 +2,14 @@
 
 using namespace std;
 
-void move(int from, int to)
-{
-	cout << from << " " << to << '\n';
-}
-
 void hanoi(int n, int from, int by, int to)
 {
 	if (n == 0)
 		return;
+	// n - 1번째 원판을 1에서 2로 옮기는 함수
 	hanoi(n - 1, from, to, by);
-	move(from, to);
+	cout << from << " " << to << '\n';
+	// n - 1번째 원판을 2에서 3으로 옮기는 함수
 	hanoi(n - 1, by, from, to);
 }
 
@@ -21,11 +18,7 @@ int main() {
 	cin >> k;
 	cin.tie(0);
 	ios::sync_with_stdio(0);
-
 	cout << (2 << k-1) - 1 << '\n';
-
 	hanoi(k, 1, 2, 3);
-
-
 	return 0;
 }
