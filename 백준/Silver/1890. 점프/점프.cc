c46a5d8bfeb19c52acaa571480a014fb763aa9cd
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2024-05-02 03:49:23 +0900

    [Silver I] Title: 점프, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	int n, graph[101][101];
+	long long dp[101][101] = {};
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			cin >> graph[i][j];
+	}
+
+	dp[0][0] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == n - 1 && j == n - 1)
+				break;
+			if (dp[i][j] != 0) {
+				int idx = graph[i][j];
+				if (j + idx < n)
+					dp[i][j + idx] += max(dp[i][j], (long long)1);
+				if (i + idx < n)
+					dp[i + idx][j] += max(dp[i][j], (long long)1);
+			}
+		}
+	}
+
+	cout << dp[n-1][n-1];
+	return 0;
+}
