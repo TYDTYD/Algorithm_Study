35f76c7e2e96b28008725ee4e8442a8d6fc53cf5
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2024-06-24 00:22:05 +0900

    [Silver IV] Title: Fujiyama Thursday, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/26317. Fujiyama Thursday/Fujiyama Thursday.cc
+++ 백준/Silver/26317. Fujiyama Thursday/Fujiyama Thursday.cc
@@ -1,30 +1,23 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 using namespace std;
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	int n;
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
-		int c, answer = 0;
-		vector<int> t,d;
+		int c, answer = 0, d[51], t[201];
 		cin >> c;
-		for (int j = 0; j < c; j++) {
-			int dist;
-			cin >> dist;
-			d.emplace_back(dist);
-		}
-		for (int j = 0; j < 4 * c; j++) {
-			int time;
-			cin >> time;
-			t.emplace_back(time);
-		}
-		sort(d.begin(), d.end(), greater<int>());
-		sort(t.begin(), t.end());
-		for (int j = 3, k = 0; k < c; j += 4, k++) {
+		for (int j = 0; j < c; j++)
+			cin >> d[j];
+		for (int j = 0; j < 4 * c; j++)
+			cin >> t[j];
+		sort(d, d + c, greater<int>());
+		sort(t, t + 4 * c);
+		for (int j = 3, k = 0; k < c; j += 4, k++)
 			answer = max(t[j] + d[k], answer);
-		}
 		cout << "Trip #" << i << ": " << answer << '\n';
 	}
 	return 0;
