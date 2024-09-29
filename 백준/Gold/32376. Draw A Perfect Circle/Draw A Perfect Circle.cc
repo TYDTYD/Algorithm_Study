d75adfb6e54840a32615c41285472282f8d8040f
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2024-09-30 05:23:55 +0900

    [Gold IV] Title: Draw A Perfect Circle, Time: 96 ms, Memory: 5236 KB -BaekjoonHub

--- 백준/Gold/32376. Draw A Perfect Circle/Draw A Perfect Circle.cc
+++ 백준/Gold/32376. Draw A Perfect Circle/Draw A Perfect Circle.cc
@@ -4,7 +4,7 @@
 #include <cmath>
 using namespace std;
 
-double dist(double x,double y) {
+double dist(double x, double y) {
 	return sqrt(x * x + y * y);
 }
 
@@ -22,12 +22,9 @@ int main() {
 	sort(v.begin(), v.end());
 	int answer = 0;
 	for (vector<double>::iterator it = v.begin(); it != v.end(); it++) {
-		
-		int result = upper_bound(v.begin(), v.end(), *it + k)
-			- lower_bound(v.begin(), v.end(), *it);
+		int result = upper_bound(v.begin(), v.end(), max(0.0, *it + k)) - it;
 		answer = max(answer, result);
 	}
-	cout.precision(10);
 	cout << fixed;
 	cout << answer * (100 / n);
 	return 0;
