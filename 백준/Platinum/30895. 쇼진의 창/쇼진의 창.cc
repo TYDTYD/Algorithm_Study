36745793bdd6fe7e567b43fc40a9dc4a26c3b692
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2024-10-12 20:30:17 +0900

    [Platinum V] Title: 쇼진의 창, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/30895. 쇼진의 창/쇼진의 창.cc
+++ 백준/Platinum/30895. 쇼진의 창/쇼진의 창.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	long long x, y, k;
+	int x, y, k;
 	cin >> x >> y >> k;
 
 	if (k <= x) {
@@ -10,34 +10,20 @@ int main() {
 		return 0;
 	}
 
-	long double result = 0;
-	int answer = 0;
-	long long withCount = 0;
-	long long count = 0;
-	if (k % x == 0)
-		count = k / x;
-	else
-		count = k / x + 1;
-
-	if (k % (x + y) == 0)
-		withCount = k / (x + y);
-	else
-		withCount = k / (x + y) + 1;
-
-	result = (long double)count / (long double)withCount;
+	double result = 0;
+	int answer, xy = x + y;
+	int count = k % x == 0 ? k / x : k / x + 1;
+	int withCount = k % xy == 0 ? k / xy : k / xy + 1;
+	
+	result = (double)count / (double)withCount;
 	answer = k;
-	long long xy = x + y;
-	long long index = 0;
-	if (k % xy == 0)
-		index = k;
-	else
-		index = (k / xy + 1) * xy;
-	index++;
+	
+	long long index = k % xy == 0 ? k+1 : (k / xy + 1) * xy + 1;
 
 	for (int i = 0; i <= x; i++) {
 		count = index % x == 0 ? index / x : index / x + 1;
 		withCount = index % xy == 0 ? index / xy : index / xy + 1;
-		long double r = (long double)count / (long double)withCount;
+		double r = (double)count / (double)withCount;
 		if (result > r) {
 			answer = index;
 			result = r;
