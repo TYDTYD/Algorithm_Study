71f017c787078e35fd57c28f83b381808d72474d
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2024-12-29 17:54:07 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main() {
+	string sentence;
+	cin >> sentence;
+	string alpha[8] = { "c=","c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+	int answer = 0;
+
+	for(int i=0; i<sentence.length(); i++){
+		bool pass = false;
+		for (const auto& a : alpha) {
+			if (sentence.length() - i >= a.size()) {
+				string cmp = "";
+				for (int j = 0; j < a.length(); j++)
+					cmp += sentence[i + j];
+				if (cmp == a) {
+					answer++;
+					i += a.size() - 1;
+					pass = true;
+					break;
+				}
+			}
+		}
+		if (pass)
+			continue;
+		answer++;
+	}
+
+	cout << answer;
+
+	return 0;
+}
