2e9f4ef6ebfe80ce05c09010bb4d27d760ac837f
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2025-01-12 18:30:56 +0900

    [Silver I] Title: 상근이의 여자친구, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4454. 상근이의 여자친구/상근이의 여자친구.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+typedef long double lld;
+using namespace std;
+
+lld calc(lld a, lld b, lld c, lld d, lld m, lld v) {
+	return m * (a * v * v * v + b * v * v + c * v + d);
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout << fixed;
+	cout.precision(2);
+
+	lld a, b, c, d, m, t;
+	while (cin >> a >> b >> c >> d >> m >> t) {
+		lld start = 0;
+		lld end = 1e8;
+		lld mid = 0;
+		while (end-start > 1e-9) {
+			mid = (start + end) / 2;
+			if (t >= calc(a, b, c, d, m, mid)) {
+				start = mid;
+			}
+			else {
+				end = mid;
+			}
+		}
+		lld answer = trunc(start * 100) / 100;
+		cout << answer << '\n';
+	}
+
+	return 0;
+}
