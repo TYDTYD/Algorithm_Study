1544c75b096f0e49e29bc04cdb29e0589417934e
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2025-03-12 22:28:59 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 60 ms, Memory: 10520 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.cc
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.cc
@@ -9,24 +9,26 @@ int main() {
 	cin.tie(0);
 	int n;
 	cin >> n;
-	long long answer = 0;
+	long long pow[300001];
+	long long plus = 0;
+	long long minus = 0;
+	long long p = 1;
 	vector<long long> v;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		long long s;
 		cin >> s;
 		v.emplace_back(s);
+		pow[n - i] = p - 1;
+		p *= 2;
+		p %= MOD;
 	}
 	sort(v.begin(), v.end());
-	
-	for (int i = 1; i < n; i++) {
-		long long count = 1;
-		for (int j = 1; j <= i; j++) {
-			count %= MOD;
-			answer += ((v[i] - v[i - j]) * count) % MOD;
-			answer %= MOD;
-			count <<= 1;
-		}
+	for (int i = 0; i < n; i++) {
+		plus += (pow[i] * v[n - 1 - i]) % MOD;
+		minus += (pow[i] * v[i]) % MOD;
+		plus %= MOD;
+		minus %= MOD;
 	}
-	cout << answer;
+	cout << (plus % MOD + MOD - minus % MOD) % MOD;
 	return 0;
 }
