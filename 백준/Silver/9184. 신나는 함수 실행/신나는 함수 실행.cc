b4e58540591e8d81b2717630d46e66866a5c363d
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2025-03-22 03:22:53 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int w[21][21][21];
+
+int solve(int a, int b, int c) {
+	if (a <= 0 || b <= 0 || c <= 0)
+		return w[0][0][0];
+	if (a > 20 || b > 20 || c > 20)
+		return solve(20, 20, 20);
+	if (w[a][b][c])
+		return w[a][b][c];
+
+	if (a < b && b < c) {
+		w[a][b][c] = solve(a, b, c - 1) + solve(a, b - 1, c - 1) - solve(a, b - 1, c);
+		return w[a][b][c];
+	}
+	w[a][b][c] = solve(a - 1, b, c) + solve(a - 1, b - 1, c) + solve(a - 1, b, c - 1) - solve(a - 1, b - 1, c - 1);
+	return w[a][b][c];
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int a, b, c;
+	w[0][0][0] = 1;
+	while (cin >> a >> b >> c) {
+		if (a == -1 && b == -1 && c == -1)
+			break;
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << solve(a,b,c) << '\n';
+	}
+	return 0;
+}
