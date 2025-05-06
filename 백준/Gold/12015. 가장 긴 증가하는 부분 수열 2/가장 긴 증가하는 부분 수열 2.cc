2f00bf6e248743f4dc69853e7bc60235a6c01296
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2025-05-06 19:55:45 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 436 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int LCS(vector<int>& list) {
+	vector<int> result;
+	for (int i = 0; i < list.size(); i++) {
+		if (result.empty() || result.back() < list[i])
+			result.push_back(list[i]);
+		else if (result.back() > list[i]) {
+			auto it = lower_bound(result.begin(), result.end(), list[i]);
+			*it = list[i];
+		}
+	}
+	return result.size();
+}
+
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> arr;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		arr.push_back(a);
+	}
+	cout << LCS(arr);
+	return 0;
+}
