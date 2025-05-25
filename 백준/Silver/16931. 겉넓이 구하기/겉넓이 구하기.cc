10cf30972b89b7e1b95dee32e2c72d990373c179
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2025-05-25 20:15:37 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.cc
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
@@ -9,8 +8,7 @@ int main() {
 	int dx[4] = { 1,0,-1,0 }, dy[4] = { 0,1,0,-1 };
 	int max_height = -1;
 	int answer = 2 * n * m;
-	bool isOneWidth = false;
-	bool isOneHeight = false;
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> cube[i][j];
@@ -18,44 +16,18 @@ int main() {
 		}
 	}
 
-	if (n == 1 && m == 1) {
-		cout << 6;
-		return 0;
-	}
-	if (n == 1) {
-		isOneWidth = true;
-	}
-	if (m == 1) {
-		isOneHeight = true;
-	}
-		
-
 	for (int h = 1; h <= max_height; h++) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (h > cube[i][j])
 					continue;
-				if (isOneWidth) {
-					if (j == 0 || j == m - 1)
-						answer += 3;
-					else
-						answer += 2;
-				}
-				else if (isOneHeight) {
-					if (i == 0 || i == n - 1)
-						answer += 3;
-					else
-						answer += 2;
-				}
-				else if ((i == 0 || i == n - 1) && (j == 0 || j == m - 1))
-					answer += 2;
-				else if (i == 0 || i == n - 1 || j == 0 || j == m - 1)
-					answer += 1;
 				for (int k = 0; k < 4; k++) {
 					int nx = i + dx[k];
 					int ny = j + dy[k];
-					if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+					if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+						answer++;
 						continue;
+					}
 					if (h > cube[nx][ny])
 						answer++;
 				}
