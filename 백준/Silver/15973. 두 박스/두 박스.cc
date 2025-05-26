f4024ad5b035db7bccfe553b6444c1ca0378eac6
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2025-05-26 20:54:11 +0900

    [Silver I] Title: 두 박스, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/15973. 두 박스/두 박스.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+struct Rect {
+	int ax, ay, bx, by;
+	Rect(int a, int b, int c, int d) : ax(a), ay(b), bx(c), by(d) {}
+};
+
+bool Bcheck(Rect& r1, Rect& r2) {
+	if ((r1.by == r2.ay || r1.ay == r2.by) && !(r1.bx <= r2.ax || r2.bx <= r1.ax))
+		return true;
+	if (r1.bx == r2.ax && !(r1.by <= r2.ay || r2.by <= r1.ay))
+		return true;
+	return false;
+}
+
+bool Ccheck(Rect& r1, Rect& r2) {
+	if (r1.ay == r2.by && r1.bx == r2.ax)
+		return true;
+	if (r1.bx == r2.ax && r1.by == r2.ay)
+		return true;
+	return false;
+}
+
+bool Dcheck(Rect& r1, Rect& r2) {
+	if (r1.bx < r2.ax)
+		return true;
+	if (r1.by < r2.ay)
+		return true;
+	if (r2.by < r1.ay)
+		return true;
+	return false;
+}
+
+void solve(Rect R1, Rect R2) {
+	if (Dcheck(R1, R2))
+		cout << "NULL";
+	else if (Ccheck(R1, R2))
+		cout << "POINT";
+	else if (Bcheck(R1, R2))
+		cout << "LINE";
+	else
+		cout << "FACE";
+}
+
+int main() {
+	int a1, b1, c1, d1, a2, b2, c2, d2;
+	cin >> a1 >> b1 >> c1 >> d1;
+	cin >> a2 >> b2 >> c2 >> d2;
+	Rect R1(a1, b1, c1, d1);
+	Rect R2(a2, b2, c2, d2);
+	if (a1 <= a2)
+		solve(R1, R2);
+	else
+		solve(R2, R1);
+
+	return 0;
+}
