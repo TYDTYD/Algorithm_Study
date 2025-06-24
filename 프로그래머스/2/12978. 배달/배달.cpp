425421b5fba6c1bcdce4acea73a3f579de0ba00a
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2025-06-24 10:11:37 +0900

    [level 2] Title: 배달, Time: 0.30 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+typedef pair<int,int> point;
+
+struct cmp{
+    bool operator()(point& x,point& y){
+        return x.second > y.second;
+    }
+};
+
+int solution(int N, vector<vector<int>> road, int K) {
+    int answer = 0;
+    int size = road.size();
+    vector<vector<point>> points(N+1);
+    int dist[51];
+    
+    fill(dist,dist+51,1e9);
+    
+    for(int i=0; i<size; i++){
+        points[road[i][0]].push_back({road[i][1],road[i][2]});
+        points[road[i][1]].push_back({road[i][0],road[i][2]});
+    }
+    
+    priority_queue<point,vector<point>,cmp> pq;
+    dist[1]=0;
+    pq.push({1,0});
+    
+    while(!pq.empty()){
+        point cur=pq.top();
+        pq.pop();
+        if(dist[cur.first]<cur.second)
+            continue;
+        for(const auto& point:points[cur.first]){
+            int next=point.first;
+            int cost=point.second + cur.second;
+            if(cost<dist[next]){
+                dist[next]=cost;
+                pq.push({next,cost});
+            }
+        }
+    }
+    
+    for(int i=1; i<=N; i++){
+        if(dist[i]<=K)
+            answer++;
+    }
+
+    return answer;
+}
