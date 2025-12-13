68fa3a55cd19e7ba42b08d6edfe3b24b045fd410
SeungMin Lee <48386074+TYDTYD@users.noreply.github.com>
2025-12-13 22:20:29 +0900

    [Silver III] Title: 카드 놓기, Time: 288 ms, Memory: 10288 KB -BaekjoonHub

---
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> commands;
+	deque<int> result;
+
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		commands.push_back(x);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		int command = commands.back();
+		commands.pop_back();
+		switch (command) {
+		case 1:
+			result.push_front(i);
+			break;
+		case 2: {
+			int idx = result.front();
+			result.pop_front();
+			result.push_front(i);
+			result.push_front(idx);
+			break;
+		}
+		case 3:
+			result.push_back(i);
+			break;
+		}
+	}
+
+	for (const auto& r : result)
+		cout << r << ' ';
+
+	return 0;
+}
